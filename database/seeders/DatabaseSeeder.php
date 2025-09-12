a30407e2a8fab367eceab301d9e6fdcab4b5641b
Legrand Seb <legrandse@gmail.com>
2025-09-12 16:51:39 +0200

    factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,25 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       /* User::factory()->create([
+            'name' => 'Legrand Sebastien',
+            'email' => 'legrandse@gmail.com',
+            
+        ]);*/
+        
+        // Préparer les paramètres par défaut
+	    $settings = [
+	        ['device' => $device->serial, 'name' => 'payconiq',   'value' => 1],
+	        ['device' => $device->serial, 'name' => 'cash',       'value' => 1],
+	        ['device' => $device->serial, 'name' => 'parity',     'value' => 1],
+	        ['device' => $device->serial, 'name' => 'tokenArray', 'value' => 1],
+	        ['device' => $device->serial, 'name' => 'ngrok',      'value' => null],
+	        ['device' => $device->serial, 'name' => 'magasin',    'value' => 0],
+	    ];
+
+	    // Insérer en une seule fois
+	    Settings::factory()->insert($settings);
+        
+        
     }
 }
