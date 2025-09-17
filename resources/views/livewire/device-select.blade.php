5358ff9d3638a15b5fe274403589715a654fdcc1
Legrand Seb <legrandse@gmail.com>
2025-09-17 16:43:02 +0200

    update

--- resources/views/livewire/device-select.blade.php
+++ resources/views/livewire/device-select.blade.php
@@ -4,7 +4,7 @@
 	    	<div class="col-sm-12 col-xl-12">
 	        	<div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
 					<select class="form-select" wire:model.live="deviceId">
-					  <option disabled value="" >Open this select menu</option>
+					  <option disabled value="" >Sélectionner un appareil</option>
 					  @foreach($devices as $device)
 					  <option value="{{ $device->device_id }}">{{ $device->ref }}</option>
 					  @endforeach
