028abbcaac5613c8f64665a048e11c83cbc3a3ae
Legrand Seb <legrandse@gmail.com>
2026-05-13 10:30:42 +0200

    update

--- routes/channels.php
+++ routes/channels.php
@@ -7,7 +7,7 @@ use App\Models\LinkedDevices;
 use App\Models\Transactions;
 use App\Models\Devices;
 
-Broadcast::channel('user.{id}', function (User $user, $id) {
+Broadcast::channel('user.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 /*
